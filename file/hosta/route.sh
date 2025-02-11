e4a57266fa1ac4487eb16591333cb77df48a990e
ovokll <104351597+ovokll@users.noreply.github.com>
2025-02-11 18:37:38 +0800

    Update route.sh

--- file/hosta/route.sh
+++ file/hosta/route.sh
@@ -1,2 +1,2 @@
 sudo ip -6 route del 2024:cd::2
-sudo ip -6 route add 2024:cd::2/128 encap seg6 mode encap segs fc00:2::a0,fc00:3::a0 dev eth0
+sudo ip -6 route add 2024:cd::2/128 encap seg6 mode encap segs fc00:2::a0,fc00:2::a0,fc00:3::a0 dev eth0
